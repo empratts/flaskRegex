e2a706755e1b5fd3c73e99fb0000442dcbbf80e4
Eric Pratts <eppgx@missouri.edu>
2026-06-14 08:20:07 -0500

    regex generator working

--- src/command.py
+++ src/command.py
@@ -2,33 +2,336 @@ from cmd2 import Cmd, Cmd2ArgumentParser, with_argparser, Choices
 import re
 import json
 import sqlite3
+import networkx
 import os
+import pyperclip
 
 FILE_PATH, _ = os.path.split(os.path.realpath(__file__))
 
 class TrackerCommands(Cmd):
 
     def __init__(self):
-        super().__init__()
+        history_file = f'{FILE_PATH}/../data/command_history.dat'
+        super().__init__(persistent_history_file=history_file, persistent_history_length=1000)
 
-        self.db_conn = sqlite3.connect(f'{FILE_PATH}/../data/item.db')
-        self.db_cur = self.db_conn.cursor()
+        db_conn = sqlite3.connect(f'{FILE_PATH}/../data/item.db')
+        db_cur = db_conn.cursor()
         self.prompt = ">"
+        self.debug = True
+
+        result = db_cur.execute("SELECT value FROM prefix")
+
+        if result:
+            self.prefix = [a[0] for a in result.fetchall()]
+        else:
+            self.prefix = ["DATABASE_ERROR"]
+        
+        result = db_cur.execute("SELECT value FROM suffix")
+
+        if result:
+            self.suffix = [a[0] for a in result.fetchall()]
+        else:
+            self.suffix = ["DATABASE_ERROR"]
+
     
     def providePrefixes(self):
-        return Choices.from_values(["Prefix"])
+        return Choices.from_values(self.prefix)
     
     def provideSuffixes(self):
-        return Choices.from_values(["Suffix"])
+        return Choices.from_values(self.suffix)
     
     def provideBases(self):
-        return Choices.from_values(["Base"])
+        db_conn = sqlite3.connect(f'{FILE_PATH}/../data/item.db')
+        db_cur = db_conn.cursor()
+
+        result = db_cur.execute("SELECT value FROM base")
+
+        if result:
+            bases = [a[0] for a in result.fetchall()]
+        else:
+            bases = ["DATABASE_ERROR"]
+
+        return Choices.from_values(bases)
+    
+    def provideWantedPrefixes(self, arg_tokens, **kwargs):
+
+        base = arg_tokens.get('base')
+        suffix = arg_tokens.get('suffix')
+
+        db_conn = sqlite3.connect(f'{FILE_PATH}/../data/item.db')
+        db_cur = db_conn.cursor()
+
+        if base:
+            base_id = getBaseIDFromDB(base[0], db_cur)
+
+            if suffix:
+                suffix_id = getSuffixIDFromDB(suffix[0], db_cur)
+                result = db_cur.execute("""SELECT prefix.value FROM wanted
+                                        INNER JOIN prefix ON wanted.prefix_id = prefix.id
+                                        WHERE base_id = ? AND suffix_id = ?""", (base_id, suffix_id))
+            else:
+                result = db_cur.execute("""SELECT prefix.value FROM wanted
+                                        INNER JOIN prefix ON wanted.prefix_id = prefix.id
+                                        WHERE base_id = ?""", (base_id,))
+        else:
+            return Choices.from_values(self.prefix)
+
+        if result:
+            prefix = [a[0] for a in result.fetchall()]
+        else:
+            prefix = ["DATABASE_ERROR"]
+
+        return Choices.from_values(prefix)
+    
+    def provideWantedSuffixes(self, arg_tokens, **kwargs):
+        base = arg_tokens.get('base')
+        prefix = arg_tokens.get('prefix')
+
+        db_conn = sqlite3.connect(f'{FILE_PATH}/../data/item.db')
+        db_cur = db_conn.cursor()
+
+        if base:
+            base_id = getBaseIDFromDB(base[0], db_cur)
+
+            if prefix:
+                prefix_id = getPrefixIDFromDB(prefix[0], db_cur)
+                result = db_cur.execute("""SELECT suffix.value FROM wanted
+                                        INNER JOIN suffix ON wanted.suffix_id = suffix.id
+                                        WHERE base_id = ? AND prefix_id = ?""", (base_id, prefix_id))
+            else:
+                result = db_cur.execute("""SELECT suffix.value FROM wanted
+                                        INNER JOIN suffix ON wanted.suffix_id = suffix.id
+                                        WHERE base_id = ?""", (base_id,))
+        else:
+            return Choices.from_values(self.suffix)
+
+        if result:
+            suffix = [a[0] for a in result.fetchall()]
+        else:
+            suffix = ["DATABASE_ERROR"]
+
+        return Choices.from_values(suffix)
+    
+    def provideWantedBases(self):
+        db_conn = sqlite3.connect(f'{FILE_PATH}/../data/item.db')
+        db_cur = db_conn.cursor()
+
+        result = db_cur.execute("""SELECT base.value FROM wanted
+                                   INNER JOIN base ON wanted.base_id = base.id""")
+
+        if result:
+            bases = [a[0] for a in result.fetchall()]
+        else:
+            bases = ["DATABASE_ERROR"]
+
+        return Choices.from_values(bases)
 
     add_parser = Cmd2ArgumentParser()
     add_parser.add_argument('base', help="Base of the item to add", choices_provider=provideBases)
     add_parser.add_argument('-p', '--prefix', required=True, help="Prefix of the item to add", choices_provider=providePrefixes)
     add_parser.add_argument('-s', '--suffix', required=True, help="Suffix of the item to add", choices_provider=provideSuffixes)
-
     @with_argparser(add_parser)
     def do_add(self, args):
-        pass
+        db_conn = sqlite3.connect(f'{FILE_PATH}/../data/item.db')
+        db_cur = db_conn.cursor()
+
+        prefix_id = getPrefixIDFromDB(args.prefix, db_cur)
+        base_id = getBaseIDFromDB(args.base, db_cur)
+        suffix_id = getSuffixIDFromDB(args.suffix, db_cur)
+
+        if base_id and prefix_id and suffix_id:
+            db_cur.execute("""INSERT INTO wanted (prefix_id, base_id, suffix_id) VALUES (?,?,?)
+                            ON CONFLICT DO UPDATE SET
+                            prefix_id = excluded.prefix_id,
+                            base_id = excluded.base_id,
+                            suffix_id = excluded.suffix_id""", (prefix_id, base_id, suffix_id))
+            db_conn.commit()
+        else:
+            self.poutput("Add Failed")
+     
+    remove_parser = Cmd2ArgumentParser()
+    remove_parser.add_argument('base', help="Base of the item to add", choices_provider=provideWantedBases)
+    remove_parser.add_argument('-p', '--prefix', required=True, help="Prefix of the item to add", choices_provider=provideWantedPrefixes)
+    remove_parser.add_argument('-s', '--suffix', required=True, help="Suffix of the item to add", choices_provider=provideWantedSuffixes)
+    @with_argparser(remove_parser)
+    def do_remove(self, args):
+        db_conn = sqlite3.connect(f'{FILE_PATH}/../data/item.db')
+        db_cur = db_conn.cursor()
+
+        prefix_id = getPrefixIDFromDB(args.prefix, db_cur)
+        base_id = getBaseIDFromDB(args.base, db_cur)
+        suffix_id = getSuffixIDFromDB(args.suffix, db_cur)
+
+        if base_id and prefix_id and suffix_id:
+            db_cur.execute("""DELETE FROM wanted WHERE
+                            prefix_id = ? AND
+                            base_id = ? AND
+                            suffix_id = ?""", (prefix_id, base_id, suffix_id))
+            db_conn.commit()
+        else:
+            self.poutput("Remove Failed")
+
+    base_parser = Cmd2ArgumentParser()
+    base_parser.add_argument('-a', '--add', help="Base to add", type=str)
+    base_parser.add_argument('-d', '--delete', help="Base to delete", choices_provider=provideBases)
+    base_parser.add_argument('-l', '--list', help="List bases", action='store_true')
+    @with_argparser(base_parser)
+    def do_base(self, args):
+        db_conn = sqlite3.connect(f'{FILE_PATH}/../data/item.db')
+        db_cur = db_conn.cursor()
+
+        if args.add:
+            db_cur.execute('INSERT INTO base (value) VALUES (?) ON CONFLICT DO NOTHING', (args.add.lower(),))
+            db_conn.commit()
+        if args.delete:
+            db_cur.execute('DELETE FROM base WHERE value = ?', (args.delete.lower(),))
+            db_conn.commit()
+        if args.list or not (args.add or args.delete):
+            result = db_cur.execute("SELECT value FROM base")
+
+            if result:
+                base = [a[0].lower() for a in result.fetchall()]
+            else:
+                base = ["DATABASE_ERROR"]
+
+            self.poutput("Bases:")
+            self.poutput("------")
+            for b in base:
+                self.poutput(b)
+            self.poutput("")
+
+    wanted_parser = Cmd2ArgumentParser()
+    wanted_parser.add_argument('-b', '--base', help="Base filter", type=str)
+    wanted_parser.add_argument('-p', '--prefix', help="Prefix filter", type=str)
+    wanted_parser.add_argument('-s', '--suffix', help="Suffix filter", type=str)
+    wanted_parser.add_argument('-o', '--sort', help="Sort order of the output", choices=["base", "prefix", "suffix"])
+    @with_argparser(wanted_parser)
+    def do_wanted(self, args):
+        db_conn = sqlite3.connect(f'{FILE_PATH}/../data/item.db')
+        db_cur = db_conn.cursor()
+
+        result = db_cur.execute("""SELECT prefix.value, base.value, suffix.value FROM wanted
+                                   INNER JOIN prefix ON wanted.prefix_id = prefix.id
+                                   INNER JOIN base ON wanted.base_id = base.id
+                                   INNER JOIN suffix ON wanted.suffix_id = suffix.id""")
+        
+        if result:
+            items = result.fetchall()
+
+            if args.sort:
+                if args.sort == "prefix":
+                    items.sort()
+                if args.sort == "base":
+                    items.sort(key=lambda x: x[1])
+                if args.sort == "suffix":
+                    items.sort(key=lambda x: x[2])
+
+            if args.prefix:
+                items = [i for i in items if i[0] == args.prefix]
+            if args.base:
+                items = [i for i in items if i[1] == args.base]
+            if args.suffix:
+                items = [i for i in items if i[2] == args.suffix]
+
+            self.poutput(f"{len(items)} wanted items:")
+            self.poutput("----------------")
+            for i in items:
+                self.poutput(f"{i[0]} {i[1]} flask {i[2]}")
+    
+    regex_parser = Cmd2ArgumentParser()
+    regex_parser.add_argument('base', help="Base of the item to generate regex for", choices_provider=provideWantedBases)
+    @with_argparser(regex_parser)
+    def do_regex(self, args):
+        db_conn = sqlite3.connect(f'{FILE_PATH}/../data/item.db')
+        db_cur = db_conn.cursor()
+
+        base_id = getBaseIDFromDB(args.base, db_cur)
+
+        result = db_cur.execute("""SELECT prefix.short, suffix.short FROM wanted
+                                   INNER JOIN prefix ON wanted.prefix_id = prefix.id
+                                   INNER JOIN suffix ON wanted.suffix_id = suffix.id
+                                   WHERE base_id = ?""", (base_id,))
+        
+        if result:
+            items = result.fetchall()
+
+            if items:
+                G = networkx.Graph()
+
+                for i in items:
+                    G.add_node((i[0], i[1]))
+                
+                for i1 in items:
+                    for i2 in items:
+                        if i1 != i2 and not G.has_edge(i1, i2) and (i1[0], i2[1]) in items and (i2[0], i1[1]) in items:
+                            G.add_edge(i1, i2)
+                
+                _, found_cliques = networkx.approximation.clique_removal(G)
+
+                regex_strings = []
+
+                for c in found_cliques:
+                    pfx = set()
+                    sfx = set()
+
+                    for flask in c:
+                        pfx.add(flask[0])
+                        sfx.add(flask[1])
+                    
+                    regex_strings.append(f"^({'|'.join(pfx)}).*({'|'.join(sfx)})$")
+
+                final_regex = "|".join(regex_strings)
+                self.poutput(f"{len(final_regex)} characters")
+
+                # Test the regex to check for false positives and false negatives
+                all_flasks = [f"{p} {args.base} flask {s}" for p in self.prefix for s in self.suffix]
+                
+                result = db_cur.execute("""SELECT prefix.value, base.value, suffix.value FROM wanted
+                                           INNER JOIN prefix ON wanted.prefix_id = prefix.id
+                                           INNER JOIN base ON wanted.base_id = base.id
+                                           INNER JOIN suffix ON wanted.suffix_id = suffix.id""")
+                
+                if result:
+                    items = result.fetchall()
+
+                wanted_flasks = [f"{i[0]} {i[1]} flask {i[2]}" for i in items if i[1] == args.base]
+
+                for f in all_flasks:
+                    if f in wanted_flasks:
+                        if not re.match(final_regex,f):
+                            self.poutput(f"ERROR: Regex failed to match wanted flask {f}")
+                    else:
+                        if re.match(final_regex,f):
+                            self.poutput(f"ERROR: Regex gives false positive on unwanted flask {f}")
+
+                self.poutput(final_regex)
+                pyperclip.copy(final_regex)
+
+            else:
+                self.poutput("No wanted flasks found for the given base")
+        
+
+
+def getBaseIDFromDB(value:str, db_cur:sqlite3.Cursor) -> int:
+    result = db_cur.execute("SELECT id FROM base WHERE value = ?", (value,))
+    if result:
+        r = result.fetchone()
+        if r:
+            return r[0]
+    return None
+
+def getPrefixIDFromDB(value:str, db_cur:sqlite3.Cursor) -> int:
+    result = db_cur.execute("SELECT id FROM prefix WHERE value = ?", (value,))
+    if result:
+        r = result.fetchone()
+        if r:
+            return r[0]
+    return None
+
+def getSuffixIDFromDB(value:str, db_cur:sqlite3.Cursor) -> int:
+    result = db_cur.execute("SELECT id FROM suffix WHERE value = ?", (value,))
+    if result:
+        r = result.fetchone()
+        if r:
+            return r[0]
+    return None
--- src/flask.py
+++ src/flask.py
@@ -1,4 +1,6 @@
+import os
 import sys
+import json
 import signal
 import sqlite3
 import command
@@ -8,21 +10,82 @@ FILE_PATH, _ = os.path.split(os.path.realpath(__file__))
 def setupDatabase():
 
     db_conn = sqlite3.connect(f'{FILE_PATH}/../data/item.db')
-    db_cur = self.db_conn.cursor()
-    
-    db_cur.execute("CREATE TABLE IF NOT EXISTS prefix (id INTEGER PRIMARY KEY, value varchar(255), UNIQUE(value))")
-    db_cur.execute("CREATE TABLE IF NOT EXISTS base (id INTEGER PRIMARY KEY, value varchar(255), UNIQUE(value))")
-    db_cur.execute("CREATE TABLE IF NOT EXISTS suffix (id INTEGER PRIMARY KEY, value varchar(255), UNIQUE(value))")
+    db_cur = db_conn.cursor()
 
-    db_cur.execute("""CREATE TABLE IF NOT EXISTS wanted(
-                id INTEGER PRIMARY KEY,
-                prefix_id INTEGER NOT NULL,
-                base_id INTEGER NOT NULL,
-                suffix_id INTEGER NOT NULL,
-                FOREIGN KEY (prefix_id) REFERENCES prefix(id),
-                FOREIGN KEY (base_id) REFERENCES base(id),
-                FOREIGN KEY (suffix_id) REFERENCES suffix(id))""")
-    db_conn.commit()
+    db_cur.execute("CREATE TABLE IF NOT EXISTS settings(name varchar(255), value varchar(255), UNIQUE(name))")
+
+    initialized = db_cur.execute("SELECT value FROM settings WHERE name='initialized'")
+    
+    if not initialized:
+        print("Database Error.")
+        sys.exit(1)
+
+    if len(initialized.fetchall()) == 0:
+            
+        db_cur.execute("CREATE TABLE IF NOT EXISTS prefix (id INTEGER PRIMARY KEY, value varchar(255), short varchar(255), UNIQUE(value))")
+        db_cur.execute("CREATE TABLE IF NOT EXISTS base (id INTEGER PRIMARY KEY, value varchar(255), UNIQUE(value))")
+        db_cur.execute("CREATE TABLE IF NOT EXISTS suffix (id INTEGER PRIMARY KEY, value varchar(255), short varchar(255), UNIQUE(value))")
+
+        db_cur.execute("""CREATE TABLE IF NOT EXISTS wanted(
+                          id INTEGER PRIMARY KEY,
+                          prefix_id INTEGER NOT NULL,
+                          base_id INTEGER NOT NULL,
+                          suffix_id INTEGER NOT NULL,
+                          FOREIGN KEY (prefix_id) REFERENCES prefix(id),
+                          FOREIGN KEY (base_id) REFERENCES base(id),
+                          FOREIGN KEY (suffix_id) REFERENCES suffix(id))""")
+        
+        with open(f'{FILE_PATH}/../data/flask_mods.json', "r", encoding='utf-8') as f:
+            affix = json.load(f)
+
+        p_name = [p["Name"].lower() for p in affix if p["Prefix"] == True]
+        s_name = [p["Name"].lower() for p in affix if p["Prefix"] == False]
+
+        p_short = []
+        
+        for pn in p_name:
+            for i in range(1, len(pn)):
+                short = pn[:i]
+                for full in p_name:
+                    if full.startswith(short) and pn != full:
+                        break
+                else:
+                    p_short.append(short)
+                    break
+            else:
+                print(f"ERROR: Failed to find short name for {pn}.")
+                p_short.append("")
+        
+        s_short = []
+
+        for sn in s_name:
+            for i in range(1, len(sn) + 1):
+                short = sn[-i:]
+                for full in s_name:
+                    if full.endswith(short) and sn != full:
+                        break
+                else:
+                    s_short.append(short)
+                    break
+            else:
+                print(f"ERROR: Failed to find short name for {sn}.")
+                s_short.append("")
+
+        if len(p_name) != len(p_short) or len(s_name) != len(s_short):
+            print("Failed calculating short names.")
+            sys.exit(2)
+
+        prefix = [p for p in zip(p_name, p_short)]
+        suffix = [s for s in zip(s_name, s_short)]
+
+        for p in prefix:
+            db_cur.execute('INSERT INTO prefix (value, short) VALUES (?, ?) ON CONFLICT DO NOTHING', p)
+        
+        for s in suffix:
+            db_cur.execute('INSERT INTO suffix (value, short) VALUES (?, ?) ON CONFLICT DO NOTHING', s)
+
+        db_cur.execute("""INSERT INTO settings (name, value) VALUES("initialized", "Valid") ON CONFLICT DO NOTHING""")
+        db_conn.commit()
 
 def main():
 
